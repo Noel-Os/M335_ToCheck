f378f90dc24b97d18cca8d17d9f71d50c4381af4
Noel-Os <osmanaj.noel0@gmail.com>
2021-12-09 09:48:44 +0100

    show tooken Photo

--- app/src/main/java/com/example/tocheck/CreateActivity.java
+++ app/src/main/java/com/example/tocheck/CreateActivity.java
@@ -10,12 +10,15 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import java.io.File;
 
 public class CreateActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView preview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +29,16 @@ public class CreateActivity extends AppCompatActivity {
     public void takePhoto(View view){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            preview = (ImageView) findViewById(R.id.preview);
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            preview.setImageBitmap(imageBitmap);
+        }
     }
 }
