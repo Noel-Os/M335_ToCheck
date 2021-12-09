a606823fc07b71ab15ff51cfc5b4513df70641aa
Noel-Os <osmanaj.noel0@gmail.com>
2021-12-09 11:13:11 +0100

    set Date for ToDo

--- app/src/main/java/com/example/tocheck/CreateActivity.java
+++ app/src/main/java/com/example/tocheck/CreateActivity.java
@@ -10,16 +10,22 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class CreateActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     ImageView preview;
+    TextView tw;
+    Date date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        tw = (TextView) findViewById(R.id.tw);
     }
 
     public void takePhoto(View view){
@@ -40,12 +46,17 @@ public class CreateActivity extends AppCompatActivity {
 
     public void openDatePicker(View view){
         DatePickerDialog dpd = new DatePickerDialog (this);
-
+        dpd.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                tw.setText(Integer.toString(dayOfMonth) + "." + Integer.toString(month) + "." + Integer.toString(year));
+                date = new Date(year, month, dayOfMonth);
+            }
+        });
         dpd.show();
     }
 
-    public void setNewDate(){
+    public void createToDo(View view){
 
     }
-
 }
