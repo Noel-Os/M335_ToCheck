3fa7e4dbbe93e08de28e574ae03baf45d68b18f5
Noel-Os <osmanaj.noel0@gmail.com>
2021-12-09 09:45:50 +0100

    open Camera to take Photo

--- app/src/main/java/com/example/tocheck/CreateActivity.java
+++ app/src/main/java/com/example/tocheck/CreateActivity.java
@@ -2,13 +2,29 @@ package com.example.tocheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+
+import java.io.File;
 
 public class CreateActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+    }
+
+    public void takePhoto(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
     }
 }
